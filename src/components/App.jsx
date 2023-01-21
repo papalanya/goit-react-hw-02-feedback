f125216d3a0427131c0d5dbbb179677b8781824a
Illia Pavlenko <illiampavlenko@gmail.com>
2023-01-21 02:02:52 +0200

    add notification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { Component } from "react";
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
-import { Statistics } from "./Statistics/Statistics";
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
   state = { good: 0, neutral: 0, bad: 0 };
@@ -33,13 +34,17 @@ export class App extends Component {
         </Section>
 
         <Section title='Statistics'>
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback}
-            positivePercentage={this.countPositiveFeedbackPercentage}
-          />
+          {this.countTotalFeedback() !== 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback!" />
+          )}
         </Section>
       </>
     )
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -34,4 +34,6 @@ Statistics.propTypes = {
     good: PropTypes.number.isRequired,
     neutral: PropTypes.number.isRequired,
     bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired,
 };
